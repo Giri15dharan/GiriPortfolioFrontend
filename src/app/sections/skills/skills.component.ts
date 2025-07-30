7f58fde696f3445173a4419ec87374f256711927
Giridharan S <918775@cognizant.com>
2025-07-30 18:28:48 +0530

    Alignment changes

--- src/app/sections/skills/skills.component.ts
+++ src/app/sections/skills/skills.component.ts
@@ -3,23 +3,33 @@ import { ApiService } from '../../services/api.service';
 import { SkillCategory } from '../../Models/portfolio.model';
 import { MatCardModule } from '@angular/material/card';
 import { MatProgressSpinner } from '@angular/material/progress-spinner';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-skills',
   standalone: true,
   imports: [MatProgressSpinner,
-    MatCardModule],
+    MatCardModule, CommonModule],
   templateUrl: './skills.component.html',
   styleUrl: './skills.component.scss'
 })
 export class SkillsComponent {
   skillCategories: SkillCategory[] = [];
   constructor(private apiService: ApiService) { }
+  isMobile(): boolean {
+    return window.innerWidth <= 786;
+  }
 
   ngOnInit(): void {
     this.apiService.getSkills().subscribe(data => {
       this.skillCategories = data;
     });
+  }
+
+  toggleFlip(skill: any): void {
+    if (window.innerWidth <= 786) {
+      skill.hover = !skill.hover;
+    }
   }
 }
